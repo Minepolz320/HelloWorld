348e9eb70e3d0f11446a22f7ab021e3e04cdf0d4
Minepolz320 <42765118+Minepolz320@users.noreply.github.com>
2018-08-28 10:38:30 +0300

    x3 best

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,10 @@
 public class MainClass {
 	public static void main(String[] args) {
 		//OOps some error
+		
+		//For best experience
+		System.out.println("Hello World");
+		System.out.println("Hello World");
 		System.out.println("Hello World");
 	}
 
