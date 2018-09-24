d60d64369637b01ff34e169ecdc1a51ecd9f35f3
Stanislav Stetsiuk <stanislav.stetsiuk@macys.com>
2018-09-24 14:30:19 +0200

    Review refactoring

--- main.js
+++ main.js
@@ -10,65 +10,47 @@ var conf = {
 				console.log("Application", this);
 				var l = this.getFromStack(0);    
                 console.log("document", document, l);
-             
-                
-                this.allocate(5);
-            
-               
-                
-                
-                this[this.getLastStackElemet()+5]=canvas.getContext("2d");
+//                 this.allocate(5);
+//                 this[this.getLastStackElemet()+5]=canvas.getContext("2d");
                 var self = this;
-                document.getElementById("1").addEventListener("click",function(){
-                     self.invokeFunc(2);
-                     self.Arginvoke([4,3]);
-                },false);
-                
-                
- 
-			},function(){
-                 var arr =  Array.from(document.getElementById("info").value.split(','));
-                
-                for( var c = 0; c < arr.length; c++ ) {
-                    this[this.getLastStackElemet()+c+1]=arr[c]-0;
-                    
-                }
-                 
-            }, function(){
-                
-                this.getFromStack(4).beginPath();
-                this.getFromStack(4).fillRect( 
-                                               this[0].pop()
-                                              ,this[0].pop()
-                                              ,this[0].pop()
-                                              ,this[0].pop()
-                                             );
-                this[0].pop().stroke();
-                
-                
-                
-            }
-            
-           
+                document.getElementById("1").addEventListener("click", function() {
+					self.invokeFunc( 3 );
+                }, false);
+			},
+			function() {
+				var arr =  Array.from(document.getElementById("info").value.split(','));
+                this.putToStack( arr );
+                this.thirdPartyFuncInvoke( arr.length, ["fillRect", 4] );
+            }, 
+            function() {
+                this.thirdPartyFuncInvoke( 0, ["beginPath", 4] );
+				this.invokeFunc(2);
+                this.thirdPartyFuncInvoke( 0, ["stroke", 4] );
+            },
+            canvas.getContext("2d")
 		]
 	]
 }
-
-
-
-
 function loaded(){
 	(function(config){
 		var heap = function(){}
 		heap.prototype = Object.create(Array.prototype);
 		heap.prototype.constructor = heap;
 		
-        heap.prototype.getLastStackElemet = function(){
+        heap.prototype.getLastStackElemet = function() {
             return this[0][this[0].length-1];
         }
         heap.prototype.getFromStack = function( value ) {
 			return this[0][this[0].length - 1 - value];
 		}
+        heap.prototype.getStackLength = function() {
+            return this[0].length;
+        }
+        heap.prototype.putToStack = function( value ) {
+	        for( var c = 0; c < value.length; c++ ) {
+				this[0].push( value[c] );
+			}
+        }
 		heap.prototype.invokeFunc = function( value ) {
 			this[value].apply( this );
 		}
@@ -80,19 +62,24 @@ function loaded(){
 				this.push( value[c] );
 			}
 		}
+		heap.prototype.thirdPartyFuncInvoke = function( lengthDataScopeInStack, arg ) {
+			if ( arg ) {
+				this.putToStack( arg );
+			}
+// 			console.log("thirdPartyFuncInvoke ", this[0].slice(this[0].length - lengthDataScopeInStack, lengthDataScopeInStack - 1), this.getFromStack( 1 ), this[this.getLastStackElemet()][this.getFromStack( 1 )]);
+			this[this.getLastStackElemet()][this.getFromStack( 1 )].apply( this[this.getLastStackElemet()], this[0].slice(this.getStackLength() - lengthDataScopeInStack - 2, lengthDataScopeInStack + 1) );
+			for( var c = 0; c < lengthDataScopeInStack + 2; c++ ) {
+				this[0].pop();
+			}
+// 			console.log('this[0]',this[0]);
+// 			this[0] = this[0].splice( this[0].length - lengthDataScopeInStack, lengthDataScopeInStack )
+		}
         heap.prototype.Arginvoke = function( value ){
-        
             for( var a = this[value[0]]+value[0] ; a > value[0] ; a-- ) {
                 this[0].push( this[a] ); 
             }
-            
             this.invokeFunc(value[1]);
         }
-        
-        
-       
-        
-        
         heap.prototype.allocate = function( value ){
             this[0].push(this.length);
             this[this.getLastStackElemet()] = value;
